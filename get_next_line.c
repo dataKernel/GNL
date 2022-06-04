3b52839f5b9ce3f85247b1f9fc8446e69a5f10b1
data_kernel <lancelot@valoo.home>
2022-06-04 10:43:18 +0100

    adding open and read procedures in the main file

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,38 @@
 /*   By: lancelot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 16:27:22 by lancelot          #+#    #+#             */
-/*   Updated: 2022/06/02 16:28:34 by lancelot         ###   ########.fr       */
+/*   Updated: 2022/06/04 10:38:26 by lancelot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//TODO-> créer l'instance pour le buffer
+//TODO-> créer var line pour stocker depuis buffer la ligne
+
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "get_next_line.h"
 
+//BUFFER def
+#define BUFFER_SIZE 5
 
+char	*get_next_line(int fd)
+{
+	char	buffer[BUFFER_SIZE];
+	int		res_open;
+	int		res_read;
+	
+	res_open = open("./test", O_RDONLY);
+	printf("checking...[FD_open] -> %i\n", res_open);
+	res_read = read(res_open, buffer, 5);//test_case|5:5|buffer:nbyte|
+	printf("checking...[B_read] -> %i\n", res_read);	
+	//returning shit data, just ***TESTIN***
+	return("lol");	
+}
 
 int		main(void)
 {
+	//TODO: some test??
+	get_next_line(1);
 	return(0);
 }
