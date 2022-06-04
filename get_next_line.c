e260a084daa86733659fefeb51bab841984e4541
data_kernel <lancelot@valoo.home>
2022-06-04 20:30:24 +0100

    utils + updating main

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,20 @@
 /*   By: lancelot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 16:27:22 by lancelot          #+#    #+#             */
-/*   Updated: 2022/06/04 10:38:26 by lancelot         ###   ########.fr       */
+/*   Updated: 2022/06/04 18:05:35 by lancelot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//TODO-> créer l'instance pour le buffer
-//TODO-> créer var line pour stocker depuis buffer la ligne
+//TODO-> créer l'ouverture du fichier (√)
+//TODO-> créer la lecture du fichier sur le FD correspondant (√)
+//TODO-> créer l'instance pour le buffer (√)
+//TODO-> remplir le buffer via read (√)
+//TODO-> remplir tout le contenu du fichier dans la stash (X)(test)
 
 #include <stdio.h>
 #include <fcntl.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include "get_next_line.h"
 
@@ -23,21 +28,26 @@
 
 char	*get_next_line(int fd)
 {
+	char	*stash;
 	char	buffer[BUFFER_SIZE];
 	int		res_open;
-	int		res_read;
-	
-	res_open = open("./test", O_RDONLY);
+
+	stash = malloc(sizeof(char) * count_size_stash(fd));
+	read(fd, buffer, BUFFER_SIZE);
+	//checking datas
 	printf("checking...[FD_open] -> %i\n", res_open);
-	res_read = read(res_open, buffer, 5);//test_case|5:5|buffer:nbyte|
-	printf("checking...[B_read] -> %i\n", res_read);	
-	//returning shit data, just ***TESTIN***
-	return("lol");	
+	printf("checking...[VAL_buffer] -> %s\n", buffer);
+	
+	return("lol");//returning shit __DATAS__(temp)
 }
 
 int		main(void)
 {
 	//TODO: some test??
-	get_next_line(1);
+	int		res_open;
+
+	res_open = open("./test", O_RDONLY);
+	get_next_line(res_open);//on donne le fd a GNL
 	return(0);
 }
+
---
+++ get_next_line_utils.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lancelot <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/04 18:06:20 by lancelot          #+#    #+#             */
+/*   Updated: 2022/06/04 18:07:49 by lancelot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdbool.h>
+#include <unistd.h>
+
+int	count_size_stash(fd)
+{
+	char	buffer[BUFFER_SIZE];
+	bool	check;
+	int		count;
+	int		i;
+
+	check = false;
+	count = 0;
+	do
+	{
+	read(fd, buffer, BUFFER_SIZE);
+	//check
+	i = 0;
+	while(buffer[i])
+		{
+			i++;			
+			if(buffer[i] != '\n')
+				count++;
+			else
+			{
+				check = true;
+				break;
+			}
+		}
+	}while(!check);
+	return(count);
+}
+
